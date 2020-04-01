a44e20b3b75912be58fff71b4e8aa0e8c191121a
unknown <komalbatra.3@gmail.com/komalbatra3>
2020-04-01 16:24:21 +0530

    changes done in modal

--- src/app/search.interface.ts
+++ src/app/search.interface.ts
@@ -1,35 +1,46 @@
+export class SearchCriteria {
+    person: number;
+    budget: number;
+    duration: number;
+    travelmode: string;
+    hoteltype: string;
+}
 
 export interface GlobalDestinationsObject {
     location: string;
     latitude: number;
     longitude: number;
-  }
+}
 
 export interface CalculatedExpenditure {
-    hotelExpenditure:number;
+    hotelExpenditure: number;
     foodExpenditure: number;
     travelExpenditure: number;
 }
 
-export interface UserParameters {
-    hotel: {
-        numberOfNights: number;
-        starRating: number;
-      };
-      travel: {
-        modeOfTravel: string;
-        driving?: {
-          typeOfEngine?: string;
-          typeOfVehicle?: string;
-        };
-        bus?:{
-            busClass?: string;
-        };
-        train?: {
-            trainClass?: string;
-        };
-      };
-      budgetPerPerson:number;
-      numberOfTravelers: number;
+export interface Hotel {
+    starRating: number;
 }
 
+export interface TravelDetails {
+    modeOfTravel: string;
+    driving?: {
+        typeOfEngine?: string;
+        typeOfVehicle?: string;
+    };
+    bus?: {
+        busClass?: string;
+    };
+    train?: {
+        trainClass?: string;
+    };
+}
+
+export interface UserParameters {
+    budgetPerPerson: number;
+    numberOfTravelers: number;
+    durationOfTravel: number;
+    hotel: Hotel;
+    travel: TravelDetails;
+
+}
