852969be3728adffc1f37fd37cf2aa4050c28b1b
unknown <komalbatra.3@gmail.com/komalbatra3>
2020-04-01 13:14:10 +0530

    added search modal

--- src/app/model/search-criteria.ts
+++ src/app/model/search-criteria.ts
@@ -5,3 +5,37 @@ export class SearchCriteria {
     travelmode: string;
     hoteltype: string;
 }
+
+export interface GlobalDestinationsObject {
+    location: string;
+    latitude: number;
+    longitude: number;
+  }
+
+export interface CalculatedExpenditure {
+    hotelExpenditure:number;
+    foodExpenditure: number;
+    travelExpenditure: number;
+}
+
+export interface UserParameters {
+    hotel: {
+        numberOfNights: number;
+        starRating: number;
+      };
+      travel: {
+        modeOfTravel: string;
+        driving?: {
+          typeOfEngine?: string;
+          typeOfVehicle?: string;
+        };
+        bus?:{
+            busClass?: string;
+        };
+        train?: {
+            trainClass?: string;
+        };
+      };
+      budgetPerPerson:number;
+      numberOfTravelers: number;
+}
