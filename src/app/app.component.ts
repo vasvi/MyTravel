ae363bf12073600779c1206d1ccaecfb6e67a313
Aayush Singh <aayush6330@gmail.com>
2020-04-06 14:09:28 +0530

    new design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My Travel';
 
-  onLocationChange(event){
+  currentLocationEnabled = false;
+
+  onLocationChange(event) {
     console.log(event);
   }
+
+  ngOnInit(): void {
+    this.enableLocation(false);
+  }
+
+  enableLocation(manuallyRequested) {
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.currentLocationEnabled = true;
+    }, (error) => {
+      if (manuallyRequested) {
+        alert('Please enable location setting on your device. If on browser, click the location icon on top and clear setting for the current site. Click here again after doing that')
+      }
+    });
+  }
+
 }
