630cac680da35f454261599b103675db3bf323da
Aayush Singh <aayush6330@gmail.com>
2020-04-20 00:54:43 +0530

    New UI Changes

--- src/app/global-search/global-search.component.ts
+++ src/app/global-search/global-search.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import {Component, ViewChild, ElementRef, Output, EventEmitter, AfterViewInit, NgZone} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {MapService} from '../services/map/map.service';
+import {SearchDataService} from '../services/search-data.serivce';
+import {Router} from '@angular/router';
+
 declare const google;
+
 @Component({
   selector: 'app-global-search',
   templateUrl: './global-search.component.html',
   styleUrls: ['./global-search.component.scss']
 })
-export class GlobalSearchComponent implements OnInit {
-  constructor() { }
-  @ViewChild('locationInput', { static: false }) locationInputViewChild: ElementRef;
+export class GlobalSearchComponent implements AfterViewInit {
+  constructor(private router: Router,
+              private ngZone: NgZone,
+              private mapService: MapService,
+              private dialog: MatDialog,
+              private searchService: SearchDataService) {
+  }
+
+  @ViewChild('locationInput', {static: false}) locationInputViewChild: ElementRef;
   @Output() onLocationChange: EventEmitter<any> = new EventEmitter();
-  
-  ngAfterViewInit(){
+  place: any = '';
+
+  ngAfterViewInit() {
     this.initAutoComplete();
   }
 
-  initAutoComplete(){
+  initAutoComplete() {
     setTimeout(() => {
-      let autoComplete = new google.maps.places.Autocomplete(this.locationInputViewChild.nativeElement);
+      const autoComplete = new google.maps.places.Autocomplete(this.locationInputViewChild.nativeElement);
       google.maps.event.addListener(autoComplete, 'place_changed', () => {
-        let place = autoComplete.getPlace();
-        this.onLocationChange.emit(place);
-      })
+        this.place = autoComplete.getPlace();
+        //  this.onLocationChange.emit(place);
+      });
     }, 300);
   }
 
-  ngOnInit() {}
-
+  routeToLocation() {
+    if (this.place) {
+      this.ngZone.run(() => {
+        const queryParamsObj = this.searchService.createLocationObject(this.place);
+        this.router.navigate(['location'], {queryParams: Object.assign({}, queryParamsObj), skipLocationChange: true});
+      });
+    }
+  }
 }
