d70f5d9cd82f12da92fe95615180bf539384f946
gauravsquadx <gauravbuzz@Gauravs-MacBook-Air.local>
2020-04-22 12:07:54 +0530

    fixed zone-route bug

--- src/app/global-search/global-search.component.ts
+++ src/app/global-search/global-search.component.ts
@@ -1,8 +1,8 @@
-import {Component, ViewChild, ElementRef, Output, EventEmitter, AfterViewInit, NgZone} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {MapService} from '../services/map/map.service';
-import {SearchDataService} from '../services/search-data.serivce';
-import {Router} from '@angular/router';
+import { Component, ViewChild, ElementRef, Output, EventEmitter, AfterViewInit, NgZone } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MapService } from '../services/map/map.service';
+import { SearchDataService } from '../services/search-data.serivce';
+import { Router } from '@angular/router';
 
 declare const google;
 
@@ -13,13 +13,13 @@ declare const google;
 })
 export class GlobalSearchComponent implements AfterViewInit {
   constructor(private router: Router,
-              private ngZone: NgZone,
-              private mapService: MapService,
-              private dialog: MatDialog,
-              private searchService: SearchDataService) {
+    private ngZone: NgZone,
+    private mapService: MapService,
+    private dialog: MatDialog,
+    private searchService: SearchDataService) {
   }
 
-  @ViewChild('locationInput', {static: false}) locationInputViewChild: ElementRef;
+  @ViewChild('locationInput', { static: false }) locationInputViewChild: ElementRef;
 
   ngAfterViewInit() {
     this.initAutoComplete();
@@ -27,11 +27,16 @@ export class GlobalSearchComponent implements AfterViewInit {
 
   initAutoComplete() {
     setTimeout(() => {
-      const autoComplete = new google.maps.places.Autocomplete(this.locationInputViewChild.nativeElement);
+      const autoComplete = new google.maps.places.Autocomplete(this.locationInputViewChild.nativeElement, {
+        types: ['(cities)'],
+        componentRestrictions: {country: 'in'}
+      });
       google.maps.event.addListener(autoComplete, 'place_changed', () => {
         //  this.onLocationChange.emit(place);
-        const queryParamsObj = this.searchService.createLocationObject(autoComplete.getPlace());
-        this.router.navigate(['location'], {queryParams: Object.assign({}, queryParamsObj), skipLocationChange: false});
+        this.ngZone.run(() => {
+          const queryParamsObj = this.searchService.createLocationObject(autoComplete.getPlace());
+          this.router.navigate(['location'], { queryParams: Object.assign({}, queryParamsObj), skipLocationChange: false });
+        })
       });
     }, 300);
   }
