ea743c3bba5fdda63b7874c8fb4e692cc41114b1
gauravsquadx <gauravbuzz@Gauravs-MacBook-Air.local>
2020-04-22 17:38:21 +0530

    added test cases

--- src/app/location/location-images/location-images.component.spec.ts
+++ src/app/location/location-images/location-images.component.spec.ts
@@ -1,16 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LocationImagesComponent } from './location-images.component';
 
 describe('LocationImagesComponent', () => {
   let component: LocationImagesComponent;
   let fixture: ComponentFixture<LocationImagesComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationImagesComponent ]
+      declarations: [LocationImagesComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +23,34 @@ describe('LocationImagesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should test navigateNext', () => {
+    component.images = ['1', '2', '3', '4'];
+    component.selectedImageIndex = 0;
+    component.navigateNext();
+    expect(component.selectedImageIndex).toEqual(1);
+    component.selectedImageIndex = 2;
+    component.navigateNext();
+    expect(component.selectedImageIndex).toEqual(3);
+  })
+
+  it('should test navigatePrev', () => {
+    component.images = ['1', '2', '3', '4'];
+    component.selectedImageIndex = 3;
+    component.navigatePrev();
+    expect(component.selectedImageIndex).toEqual(2);
+    component.selectedImageIndex = 1;
+    component.navigatePrev();
+    expect(component.selectedImageIndex).toEqual(0);
+  })
+
+  it('should test image popup behaviour', () => {
+    component.images = ['1', '2', '3'];
+    component.closeDialog();
+    expect(component.displayDialog).toEqual(false);
+
+    component.openDialog(2);
+    expect(component.selectedImageIndex).toEqual(2);
+    expect(component.displayDialog).toEqual(true);
+  })
 });
