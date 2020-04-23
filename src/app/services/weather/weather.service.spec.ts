199fc97ed2ed9a9c86894f8728fd783ed7588cd6
Vasvi <vasvi@github.com>
2020-04-23 19:11:04 +0530

    weather servcice unit tests

--- src/app/services/weather/weather.service.spec.ts
+++ src/app/services/weather/weather.service.spec.ts
@@ -1,56 +1,26 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpService } from '../http/http.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WeatherService } from './weather.service';
 import { WeatherDataMock, GetWeatherDetailsOptions } from '../../mockData/weather-mock-data';
-import { of, Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { of } from 'rxjs';
 
 describe('WeatherService', () => {
-  let weatherService: WeatherService;
-  let httpService: HttpService;
-
-//   beforeEach(async(() => {
-//     weatherService = new WeatherService(httpService);
-// }));
-
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [WeatherService, HttpService]
-  });
-
-  weatherService = TestBed.get(WeatherService);
-})
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [WeatherService, HttpService]
+   });
+ })
 
 
   it('should be created', () => {
+    const weatherService = TestBed.get(WeatherService);
     expect(weatherService).toBeTruthy();
   });
 
-  // xit('should test getWeatherDetails', () => {
-  //   httpService = TestBed.get(httpService);
-
-  //   let response = {
-  //     daily: WeatherDataMock.daily,
-  //     current: WeatherDataMock.current
-  //   };
-
-  //   spyOn(httpService, 'makeGetRequest').and.callFake(() => {
-  //     return of(WeatherDataMock)
-  //   });
-
-  //   let output = weatherService.getWeatherDetails(GetWeatherDetailsOptions);
-  //   expect(output).toEqual(of(response));
-  // });
-
-  it('should test getWeatherDetails with cached data', inject([HttpTestingController, WeatherService, HttpService],
-    (httpMock: HttpTestingController, weatherService: WeatherService, httpService) => {
-
-      let url = `${environment.OPENWEATHER.api_endpoint}/onecall?lat=${GetWeatherDetailsOptions.geometry[0]}&lon=${GetWeatherDetailsOptions.geometry[1]}&appid=${environment.OPENWEATHER.appid}&units=metric`
-      spyOn(httpService, 'makeGetRequest').and.callFake(() => {
-          return httpMock.expectOne(url);
-      });
+  it('should test getWeatherDetails with cached data', inject([WeatherService],
+    (weatherService: WeatherService) => {
       weatherService.cacheWeatherDetails = {
         'ChIJP9A_FgiHBDkRzXZQvg6oKYE': {
           daily: WeatherDataMock.daily,
@@ -59,11 +29,33 @@ beforeEach(() => {
       }
 
       weatherService.getWeatherDetails(GetWeatherDetailsOptions).subscribe((data) => {
-        console.log(data);
         expect(data).toEqual({
           daily: WeatherDataMock.daily,
           current: WeatherDataMock.current
         })
       });
     }));
+
+    it('should test getWeatherDetails with no cached data', inject([WeatherService, HttpService],
+      (weatherService: WeatherService, httpService: HttpService) => {
+
+        weatherService.cacheWeatherDetails = {
+          'JP9A_FgiHBDkRzXZQvg6oKYE': {
+            daily: WeatherDataMock.daily,
+            current: WeatherDataMock.current
+          }
+        }
+
+        spyOn(httpService, 'makeGetRequest').and.callFake(() => {
+            return of(WeatherDataMock);
+        });
+
+        weatherService.getWeatherDetails(GetWeatherDetailsOptions).subscribe((data) => {
+          console.log(data);
+          expect(data).toEqual({
+            daily: WeatherDataMock.daily,
+            current: WeatherDataMock.current
+          })
+        });
+      }));
 });
