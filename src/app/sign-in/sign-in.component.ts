877d41b62a25a129cba72ffd6c883dfd8e66ddd5
Vasvi <vasvi@github.com>
2020-04-29 20:41:40 +0530

    updating scope

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService, GoogleLoginProvider} from 'angularx-social-login';
+import {AuthService, GoogleLoginProvider, LoginOpt} from 'angularx-social-login';
 
 @Component({
   selector: 'sign-in',
@@ -11,6 +11,9 @@ export class SignInComponent implements OnInit {
   loggedIn;
   menuOpen = false;
   userName = 'userName';
+  scope: LoginOpt = {
+    scope: 'https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly '
+  }
 
   constructor(
     private authService: AuthService
@@ -25,7 +28,7 @@ export class SignInComponent implements OnInit {
   }
 
   signInWithGoogle = (): void => {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID, this.scope);
   }
 
   signOut = (): void => {
