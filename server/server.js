19d16c2accbed3901c9ba8675ce935ad71f803a4
gauravsquadx <gauravbuzz@Gauravs-MacBook-Air.local>
2020-04-30 12:06:57 +0530

    added server & config

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static(__dirname + '/public/'));
+
+app.use((req, res, next) => {
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
