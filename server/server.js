580e21052ca8cec6e66991469676544fb884da0f
gauravsquadx <gauravbuzz@Gauravs-MacBook-Air.local>
2020-04-30 12:57:34 +0530

    restored default

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res, next) => {
-    res.send('hello from cloudbuild');
+app.use(express.static(__dirname + '/public/'));
+
+app.use((req, res, next) => {
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(__dirname+'/public/index.html');
 })
-
-// app.use(express.static(__dirname + '/public/'));
-
-// app.use((req, res, next) => {
-//     res.setHeader('content-type', 'text/html');
-//     res.sendFile(__dirname+'/public/index.html');
-// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
