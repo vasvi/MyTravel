e9dddaf1584bc2a5b1ad65134f813ce03a559359
Vasvi <vasvi@github.com>
2020-05-04 19:56:53 +0530

    adding api key for google cloud project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,12 +11,13 @@ export const environment = {
     TRAVEL: {
       name: 'MyTravelMaps',
       client_id: '207524266129-eofqorqkgmbduk96ur44rukok58r0pla.apps.googleusercontent.com',
-      project_id: 'travel-272407',
+      apiKey: 'AIzaSyC5-HvS8pMo3xEKtt6SlrC0J7-vfjLP9nE',
+      project_id: 'mytravelmaps',
     },
     MY_MAPS: {
       name: 'My Maps Project',
       apiKey: 'AIzaSyB19_zfT_vKvOlGa0pz0uQqPh2L_eHleUI',
-      project_id: 'ivory-lotus-272709'
+      project_id: 'totemic-vim-276208'
     }
   },
   OPENWEATHER: {
